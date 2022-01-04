0012cbc647b79d48d35e6fcb37db003641e0f9ec
trustedhuy <huy.pham@trustingsocial.com>
2022-01-04 11:40:53 +0700

    add env package

---
+++ env/env.go
@@ -0,0 +1,11 @@
+package env
+
+import "github.com/spf13/viper"
+
+func GetWithDefault[T any](key string, value T) T {
+	viper.SetDefault(key, value)
+	if v, ok := viper.Get(key).(T); ok {
+		return v
+	}
+	return value
+}
